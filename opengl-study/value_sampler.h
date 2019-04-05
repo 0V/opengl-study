f9dfc86c80092d566aedb9b10fa0166812ff3af5
G2X <g2simm@gmail.com>
2019-04-05 19:08:46 +0900

    initial commit

---
+++ opengl-study/value_sampler.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <random>
+
+template <typename T>
+class ValueSampler
+{
+private:
+	using ThisType = ValueSampler<T>;
+	mutable std::random_device seed_gen_;
+	mutable std::mt19937 engine_ = std::mt19937(seed_gen_());
+	mutable std::uniform_real_distribution<T> dist_;
+
+public:
+	ValueSampler(ThisType&& o) noexcept { }
+	ValueSampler(T min, T max) : dist_(std::uniform_real_distribution<T>(min, max)) {}
+
+	double sample() const
+	{
+		return dist_(engine_);
+	}
+};
+
+template <>
+class ValueSampler<int>
+{
+private:
+	using ThisType = ValueSampler<int>;
+	mutable std::random_device seed_gen_;
+	mutable std::mt19937 engine_ = std::mt19937(seed_gen_());
+	mutable std::uniform_int_distribution<int> dist_;
+
+public:
+	ValueSampler(ThisType&& o) noexcept { }
+	ValueSampler(int min, int max) : dist_(std::uniform_int_distribution<int>(min, max)) {}
+
+	double sample() const
+	{
+		return dist_(engine_);
+	}
+};
